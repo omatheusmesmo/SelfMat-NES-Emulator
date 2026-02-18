0ebbc2f84cfca6f5b9f299f7e23e9ab2a03a7ec6
Matheus Oliveira <matheus.6148@gmail.com>
2026-02-18 16:45:04 -0300

    feat(cartridge): add Mapper integration for CPU read/write operations (#5, #32)

--- src/main/java/dev/omatheusmesmo/selfmat/nes/emulator/core/rom/Cartridge.java
+++ src/main/java/dev/omatheusmesmo/selfmat/nes/emulator/core/rom/Cartridge.java
@@ -1,9 +1,20 @@
 package dev.omatheusmesmo.selfmat.nes.emulator.core.rom;
 
+import dev.omatheusmesmo.selfmat.nes.emulator.core.rom.mappers.Mapper;
+
 public record Cartridge(
         NESFileHeader NESFileHeader,
         byte[] prgRomData,
         byte[] chrRomData,
-        byte[] trainerData
+        byte[] trainerData,
+        Mapper mapper
 ) {
+
+    public byte cpuRead(int address){
+        return mapper.cpuRead(address);
+    }
+
+    public void cpuWrite(int address, byte cpu){
+        mapper.cpuWrite(address, cpu);
+    }
 }
